6b5c29bd56e11f3c2d560c68aecb309b5a9e12c5
Lydia Cho <lydiacho@gse.havard.edu>
2020-09-07 00:53:10 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -5,13 +5,13 @@ board = "board"
 row_1 = [" ", " ", " "]
 
 # Position 1: Left
-boardrow_1[0]
+row_1[0]
 
 # Position 2: Middle
-boardrow_1[1]
+row_1[1]
 
 # Position 3: Right
-boardrow_1row_1[2]
+row_1[2]
 
 
 # Row 2 
