b2be53afbfc01036e894493b3c9949ba5c395b9b
Lydia Cho <lydiacho@gse.havard.edu>
2020-09-07 00:50:02 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,40 @@
 # Define the variable board below.
 Array = "board"
+
+# Row 1 
+row_1 = [" ", " ", " "]
+
+# Position 1: Left
+row_1[0]
+
+# Position 2: Middle
+row_1[1]
+
+# Position 3: Right
+row_1[2]
+
+
+# Row 2 
+row_2 = [" ", " ", " "]
+
+# Position 1: Left
+row_2[0]
+
+# Position 2: Middle
+row_2[1]
+
+# Position 3: Right
+row_2[2]
+
+
+# Row 3
+row_3 = [" ", " ", " "]
+
+# Position 1: Left
+row_3[0]
+
+# Position 2: Middle
+row_3[1]
+
+# Position 3: Right
+row_3[2]
